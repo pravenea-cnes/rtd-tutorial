cad782ec9078bff0b2c8b7d3d9142cc091a0fb47
Patrice Raveneau <patrice.raveneau@cnes.fr>
2021-10-13 12:05:48 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Dambox'
+copyright = '2020, CNES'
+author = 'CNES'
 
 release = '0.1'
 version = '0.1.0'
